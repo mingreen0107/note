75701a42abfe89bdaad46ce144fdc6c11045debe
minju <71gnos@naver.com>
2024-03-09 02:21:33 +0900

    P

--- lib/main.dart
+++ lib/main.dart
@@ -156,7 +156,7 @@ class CustomListView extends StatelessWidget {
                                   content: value,
                                   createAt: items[index].createAt));
                             }
-              });
+                  });
             },
           );
         });
